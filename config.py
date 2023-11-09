8210cc7e5ab1704022436121994c4f128987f0b8
Nathan De Los Santos <nathan.edlsantos@gmail.com>
2023-11-08 17:45:11 -0800

    added config file

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 RECIPES_PATH = "O:/recipe_recommendation/food-dataset-en.csv"
+PARSED_PATH = "O:/recipe_recommendation/df_parsed.csv"
+
+TFIDF_ENCODING_PATH = "O:/recipe_recommendation/models/tfidf_encodings.pkl"
+TFIDF_MODEL_PATH = "O:/recipe_recommendation/models/tfidf.pkl"
