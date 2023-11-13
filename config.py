ef1870ab2c8aa49144be73426a0a25974e8ec6d8
Nathan De Los Santos <nathan.edlsantos@gmail.com>
2023-11-12 18:53:20 -0800

    changed config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-RECIPES_PATH = "O:/recipe_recommendation/food-dataset-en.csv"
-PARSED_PATH = "O:/recipe_recommendation/df_parsed.csv"
+RECIPES_PATH = "./food-dataset-en.csv"
+PARSED_PATH = "./df_parsed.csv"
 
-TFIDF_ENCODING_PATH = "O:/recipe_recommendation/models/tfidf_encodings.pkl"
-TFIDF_MODEL_PATH = "O:/recipe_recommendation/models/tfidf.pkl"
+TFIDF_ENCODING_PATH = "models/tfidf_encodings.pkl"
+TFIDF_MODEL_PATH = "models/tfidf.pkl"
