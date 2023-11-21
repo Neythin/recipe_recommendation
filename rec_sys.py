3f9ca31bfc7c584f76d6d7b1dab6fd75f9e6c875
Nathan De Los Santos <nathan.edlsantos@gmail.com>
2023-11-21 12:00:22 -0800

    Added addtional comments

--- rec_sys.py
+++ rec_sys.py
@@ -12,17 +12,23 @@ import ast
 def get_recommendations(N, scores):
     df_recipes = pd.read_csv(config.PARSED_PATH)
     
-    # 
+    # For a list of scores, sort in descending order and assign to variable "top"
     top = sorted(range(len(scores)), key = lambda i: scores[i], reverse = True)[:N]
+    # Create a DataFrame containing the recipe name, ingredients, score, and url
     recommendation = pd.DataFrame(columns = ['recipe', 'ingredients', 'score', 'url'])
+    # Initialize a count variable
     count = 0
 
     for i in top:
+        # Extracts the recipe name and stores it in the recipe column of the new dataframe
         recommendation.at[count, 'recipe'] = title_parser(df_recipes['name'][i])
+        # Extracts and parses the ingredients, storing the result in the ingredient column
         recommendation.at[count, 'ingredients'] = ingredient_parser_final(
             df_recipes['ingredient'][i]
         )
+        # Retrieves the recipe URL from the dataframe and stores it
         recommendation.at[count, 'url'] = df_recipes['link'][i]
+        # Converts the similarity score to a string with three decimal places and stores it in the score column
         recommendation.at[count, 'score'] = '{:.3f}'.format(float(scores[i]))
         count += 1
 
@@ -30,40 +36,52 @@ def get_recommendations(N, scores):
 
 def ingredient_parser_final(ingredient):
 
+    # If ingredients are in the format of a list, assign it to the ingredients variable
     if isinstance(ingredient, list):
         ingredients = ingredient
+    # If not, convert the string to a list
     else:
         ingredient = strToList(ingredient)
         ingredients = ast.literal_eval(ingredient)
 
+    # Join the list of ingredients into a single string, separate with commas
     ingredients = ",".join(ingredients)
+    # Get rid of accents
     ingredients = unidecode.unidecode(ingredients)
 
     return ingredients
 
 def title_parser(title):
+    # Get rid of accents in the title
     title = unidecode.unidecode(title)
     return title
 
 def RecSys(ingredients, N=5):
     
+    # Opens two files containing the TF-IDF encodings and the TF-IDF model
     with open(config.TFIDF_ENCODING_PATH, 'rb') as f:
         tfidf_encodings = pickle.load(f)
 
     with open(config.TFIDF_MODEL_PATH, 'rb') as f:
         tfidf = pickle.load(f)
 
+    # Tries to parse the input ingredients, if there is an exception, catch it and parse a list containing the input ingredients as a single element
     try:
         ingredients_parsed = ingredient_parser(ingredients)
     except:
         ingredients_parsed = ingredient_parser([ingredients])
 
+    # Join the parsed ingredients into a single string
     ingredients_parsed = " ".join(ingredients_parsed)
+    # Applies the pre-trained TF-IDF model to transform the joined ingredients into a TF-IDF representation
     ingredients_tfidf = tfidf.transform([ingredients_parsed])
 
+    # Calculates the cosine similarity between the TF-IDF represenation of the input ingredients and the TF-IDF encodings of a set of recipes
     cos_sim = map(lambda x: cosine_similarity(ingredients_tfidf, x), tfidf_encodings)
+    # Converts the resulting map object to a list of cosine similarity scores
     scores = list(cos_sim)
 
+    # Get the list of recommendations
     recommendations = get_recommendations(N, scores)
     return recommendations
 
--- recipe_word2vec.py
+++ recipe_word2vec.py
@@ -18,44 +18,60 @@ from rec_sys import *
 
 
 
+# Sort a corpus of documents in alphabetical order, so that ingredients put in a different order don't have different contexts
 def get_and_sort_corpus(data):
+    # Assign an empty list to be filled with sorted documents
     corpus_sorted = []
 
-    # for each list of ingredients, sort the ingredients by ascending order and add it to the new list
+    # for each word in the parsed items column of the data set, sort the words by ascending order and add it to the new list
     for doc in data.parsed.values:
         doc.sort()
         corpus_sorted.append(doc)
     
     return corpus_sorted
 
+
+
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word_model):
         self.word_model = word_model
         self.vector_size = word_model.wv.vector_size
 
+    # Define a function to reference to the instance of the class, helps with method chaining
     def fit(self):
         return self
     
+    # Assign a variable that contains a 2D array where each row represents the mean vector of a document
     def transform(self, docs):
         doc_word_vector = self.word_average_list(docs)
         return doc_word_vector
     
+    # Define a function to return a single vector representing the average of all word vectors
     def word_average(self, sent):
+        # Initialize empty list to store word vectors
         mean = []
 
+        # For each word in a list of words... 
         for word in sent:
+            # If the word exists in the vocabulary of the word model, add the word vector to the mean list
             if word in self.word_model.wv.index_to_key:
                 mean.append(self.word_model.wv.get_vector(word))
 
+        # If the mean list is empty, return an array of zeros with a size equal to the vector size
         if not mean:
             return np.zeros(self.vector_size)
+        # If the mean list is not empty, convert the list to an array and calculate the mean along the columns
         else:
             mean = np.array(mean).mean(axis=0)
             return mean
         
+    # Self = a reference to the instance of the class; docs = a list of lists, where each inner list represents a document of words
     def word_average_list(self, docs):
+        # With a list of documents, calculate the word average for each document, return a list of mean vectors, and return them in a 2D array
         return np.vstack([self.word_average(sent) for sent in docs])
     
+
+
 class TfidfEmbeddingVectorizer(object):
     def __init__(self, word_model):
         self.word_model = word_model
@@ -63,50 +79,72 @@ class TfidfEmbeddingVectorizer(object):
         self.vector_size = word_model.wv.vector_size
 
     def fit(self, docs):
+        # Initialize an empty list to add all the documents
         text_docs = []
         for doc in docs:
             text_docs.append(" ".join(doc))
 
+        # Initialize a vectorizer that converts text documents into a TF-IDF representation (Term Frequency = Inverse Document Frequency)
         tfidf = TfidfVectorizer()
+        # Fit determines the vocabulary and computes the IDF weights
         tfidf.fit(text_docs)
 
+        # Calculates the max IDF value from the model
         max_idf = max(tfidf.idf_)
+        # Creates a dictionary using defaultdict and sets max_idf to default value, the dictionary pairs each word with its IDF weight
         self.word_idf_weight = defaultdict(
             lambda: max_idf,
             [(word, tfidf.idf_[i]) for word, i in tfidf.vocabulary_.items()],
         )
         return self
     
+    # Assign a variable that contains a 2D array where each row represents the mean vector of a document
     def transform(self, docs):
         doc_word_vector = self.word_average_list(docs)
         return doc_word_vector
     
+    # Define a function to return a single vector representing the average of all word vectors
     def word_average(self, sent):
+        # Initialize empty list to store word vectors
         mean = []
+
+        # For each word in a list of words...
         for word in sent:
+            # If the word exists in the vocabulary of the word model, scale the word vector by its IDF weight and append to list
             if word in self.word_model.wv.index_to_key:
                 mean.append(
                     self.word_model.wv.get_vector(word) * self.word_idf_weight[word]
                 )
 
+        # If the mean list is empty, return an array of zeros with a size equal to the vector size
         if not mean:
             return np.zeros(self.vector_size)
+        # If the mean list is not empty, convert the list to an array and calculate the mean along the columns
         else:
             mean = np.array(mean).mean(axis=0)
             return mean
-        
+
+    # Self = a reference to the instance of the class; docs = a list of lists, where each inner list represents a document of words    
     def word_average_list(self, docs):
+        # With a list of documents, calculate the word average for each document, return a list of mean vectors, and return them in a 2D array
         return np.vstack([self.word_average(sent) for sent in docs])
     
+
+
 def get_recs(ingredients, N=5, mean=False):
+    # Load a pre-trained Word2Vec model
     model = Word2Vec.load('models/model_cbow.bin')
+    # Initializes the model, which includes normalizing vectors, making subsequent operations more memory-efficient
     model.init_sims(replace=True)
     if model:
         print('Successfully loaded model')
     data = pd.read_csv('df_parsed.csv')
+    # Parse the ingredients and create new column for it
     data['parsed'] = data.ingredient.apply(ingredient_parser)
+    # Sorts a corpus from the parsed ingredients data frame
     corpus = get_and_sort_corpus(data)
 
+    # Depending on the mean, the function chooses between creating document vectors using either mean embedding or TF-IDF embedding
     if mean:
         mean_vec_tr = MeanEmbeddingVectorizer(model)
         doc_vec = mean_vec_tr.transform(corpus)
@@ -119,6 +157,7 @@ def get_recs(ingredients, N=5, mean=False):
         doc_vec = [doc.reshape(1, -1) for doc in doc_vec]
         assert len(doc_vec) == len(corpus)
 
+    # The input ingredients are processed by splitting them into a list, parsing them, and embedding them using either mean or TF=IDF embedding
     input = ingredients
     input = input.split(",")
     input = ingredient_parser(input)
@@ -128,11 +167,16 @@ def get_recs(ingredients, N=5, mean=False):
     else:
         input_embedding = tfidf_vec_tr.transform([input])[0].reshape(1, -1)
 
+    # Calculates the cosine similarity between the input embedding and each document vector in the corpus
     cos_sim = map(lambda x: cosine_similarity(input_embedding, x)[0][0], doc_vec)
+    # Converts the cosine similarity score to a list
     scores = list(cos_sim)
+    # Returns the list of recommendations
     recommendations = get_recommendations(N, scores)
     return recommendations
     
+
+
 if __name__ == '__main__':
     input = 'chicken thigh, risdlfgbviahsddsagv, onion, rice noodle, seaweed nori sheet, sesame, shallot, soy, spinach, star, tofu'
     rec = get_recs(input)
